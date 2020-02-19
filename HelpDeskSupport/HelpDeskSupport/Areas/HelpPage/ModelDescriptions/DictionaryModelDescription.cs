aea3eeca268114866eba1516067b9f3a1e746a0d
yprim <priscipo@gmail.com>
2020-02-18 19:25:51 -0600

    se agrega el script de la base de datos

--- HelpDeskSupport/HelpDeskSupport/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace HelpDeskSupport.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- HelpDeskSupport/HelpDeskSupport/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace HelpDeskSupport.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
